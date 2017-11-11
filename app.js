c417c377840a1d63b4ca679976b0d0bdd5aecfce
Ganesh <jganesh3@gmail.com>
2017-11-10 18:43:54 -0600

    New Static route has been added. Now Basic editor is working fine.
    app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ var users = require('./routes/users');
 
 var app = express();
 
+//This will enable static HTML files
+//express.static(root, [index])
+app.use('/scripts', express.static(__dirname + '/node_modules'));
+app.use(express.static('public/HTML'))
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var fs=require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/simple',function (req,res,next) {
+  res.render('simple');
+
+})
+
+
+router.get('/list',function (req,res,next) {
+   var directory="data";
+   var files= fs.readdirSync(directory);
+   //console.log(files);
+    //res.send(JSON.stringify(files));
+    res.render('list')
+
+
+
+
+})
+
 
 module.exports = router;
